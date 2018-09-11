e54a1a8d157d9b7bdf4c4fede6cf78b3ed17f0a6
SergeySkakun <thexfiles116@gmail.com>
2018-09-11 11:36:28 +0300

    love-triangle/feature: implement function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let counterOfSpichonees = preferences.length;
+  let counterOfLoveTriangles = 0;
+
+  if (counterOfSpichonees < 3) {
+    return 0;
+  }
+
+  for (let i = 0; i < counterOfSpichonees; i++) {
+    let second = preferences[i] - 1;
+
+    if (second != i) {
+      let third = preferences[second] - 1;
+
+      if (preferences[third] - 1 == i) {
+        counterOfLoveTriangles++;
+        preferences[i] = -1;
+        preferences[second] = -1;
+        preferences[third] = -1;
+      }
+    }
+  }
+
+  return counterOfLoveTriangles;
 };
